cf62973bb38eea007374d62f0fbf7fcaea091d23
Bruno Fernando dos Santos <25.00810-3@maua.br>
2025-10-28 19:59:27 -0300

    feat: inicia o código do banco de dados no index.js

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1,1 +1,23 @@
-console.log('Hello, NodeJS')
+
+const express = require ('express')
+const cors = require ('cors')
+const mongoose = require ('mongoose')
+const app = express()
+app.use(express.json())
+app.use(cors())
+
+
+async function conectarAoMongoDB() {
+await
+mongoose.connect(`mongodb+srv://bruno_santos:<senha>@rokuzen.ncrtj8k.mongodb.net/?appName=rokuzen`)
+}
+
+app.listen(3000, () => {
+try{
+conectarAoMongoDB()
+console.log("up and running")
+}
+catch (e){
+console.log('Erro', e)
+}
+})
