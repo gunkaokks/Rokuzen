4573235a752e34ec3a6cf2e90d63eea1739fb549
Bruno Fernando dos Santos <25.00810-3@maua.br>
2025-11-14 17:32:31 -0300

    fix: tira a dependencia do mongoose-unique-validator e mexe no usuarios.js

--- ModeloRelacional/usuarios.js
+++ ModeloRelacional/usuarios.js
@@ -1,5 +1,4 @@
 const mongoose = require('mongoose');
-const uniqueValidator = require('mongoose-unique-validator')
 
 const usuarioSchema = mongoose.Schema({
   nome: { type: String, required: true },
@@ -8,8 +7,7 @@ const usuarioSchema = mongoose.Schema({
   telefone: { type: String, required: true, unique: true },
   tipo: { type: String, enum: ['master', 'gerente', 'recepcao', 'terapeuta', 'usuario'], default: 'usuario' },
   data_criacao: { type: Date, default: Date.now }
-})
-usuarioSchema.plugin(uniqueValidator)
-const Usuario = mongoose.model("Usuario", usuarioSchema)
+});
 
-const sessoesAtivas = {}
+const Usuario = mongoose.model("Usuario", usuarioSchema);
+const sessoesAtivas = {};
